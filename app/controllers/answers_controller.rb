357692fce3554e09110794d1c2cc4f3b42220a41
Rupesh Bansal <rupesh@Rupeshs-MacBook-Pro.local>
2016-01-17 18:33:00 +0530

    Wallet integration phase 1

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,17 @@
+require 'httparty'
 class AnswersController < ApplicationController
   def add
   	Answer.create(:body => params[:answer] , :question_id => params[:question_id] , :user_id => session[:user_id])
 
   	redirect_to show_question_path(:question_id => params[:question_id])
+  end
+
+  def fetchJson
+ #  	response = HTTParty.get('http://www.w3schools.com/json/myTutorials.js')
+	# json = JSON.parse(response.body)
+	debugger
+	#var a = HTTParty.get("https://github.com" , {})
+	safeurl = URI.parse(URI.encode("https://github.com", "[]"))
+  	debugger
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
 
   get     'show_question' => 'questions#show'
   get     'add_answer' => 'answers#add'
+
+  get     'fetch_json' => 'answers#fetchJson'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
