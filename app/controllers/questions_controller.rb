740cc2a796d281bedf5ab10285e279333ca473d1
shiwangi <shiwangi@shiwangi-Lenovo-Z50-70.(none)>
2016-01-18 13:07:56 +0530

    show transaction changes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -37,7 +37,8 @@ class QuestionsController < ApplicationController
 
   def show_status
      proxy = URI("http://10.3.100.207:8080")
-     options =   {http_proxyaddr: proxy.host,http_proxyport:proxy.port}
+     #options =   {http_proxyaddr: proxy.host,http_proxyport:proxy.port}
+     option = {}
      address = '1qMBuZnrmGoAc2MWyTnSgoLuWReDHNYyF'
      key = '5adb0a94a6eb60cb3b8a626e774156521b2ab964'
      url = "https://api.blocktrail.com/v1/btc/address/#{address}/transactions?api_key=#{key}"
